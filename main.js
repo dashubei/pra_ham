7597569e5df9c6656d5747be75777490225e5385
dashubei <dashubei6@gmail.com>
2023-11-20 17:18:29 +0900

    first commit

--- main.js
+++ main.js
@@ -4,6 +4,8 @@
     const open = document.getElementById('open');
     const close = document.getElementById('close');
     const overLay = document.querySelector('.sp-overlay');
+    const spans = document.querySelectorAll('.ham');
+    const pra = document.getElementById('pra');
 
     open.addEventListener('click', () => {
         overLay.classList.add('active-overlay');
@@ -11,5 +13,11 @@
 
     close.addEventListener('click', () => {
         overLay.classList.remove('active-overlay');
+    });
+
+    pra.addEventListener('click', () => {
+        spans.forEach((span) => {
+            span.classList.toggle('active');
+        });
     });
 }

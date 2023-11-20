b1fe1de22537682281b96c323ac4d2c6f8481a1b
dashubei <dashubei6@gmail.com>
2023-11-20 17:30:45 +0900

    ハンバーガーメニュー編集

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 'use strict';
 
 {
-    const open = document.getElementById('open');
-    const close = document.getElementById('close');
+
     const overLay = document.querySelector('.sp-overlay');
     const spans = document.querySelectorAll('.ham');
     const pra = document.getElementById('pra');
-
-    open.addEventListener('click', () => {
-        overLay.classList.add('active-overlay');
-    });
-
-    close.addEventListener('click', () => {
-        overLay.classList.remove('active-overlay');
-    });
-
+    
     pra.addEventListener('click', () => {
         spans.forEach((span) => {
             span.classList.toggle('active');
+            overLay.classList.toggle('active-overlay');
         });
     });
 }
